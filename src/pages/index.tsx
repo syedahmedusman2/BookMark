d7aaa3657389c87a74dd521f33f31b00f96b5315
syedahmedusman2 <66121118+syedahmedusman2@users.noreply.github.com>
2020-12-14 21:23:44 +0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -113,7 +113,7 @@ export default function Home() {
           <div className="data-div">
             {data !== undefined || (null && data.bookmarks.length !== 0)
               ? data.bookmarks.map((v, i) => (
-                  <div key={i} className="div2" style={{color: 'white', width:"200px", height: '150px', borderRadius: '50%'}}>
+                  <div key={i} className="div2" style={{color: 'lightblue', width:"200px", height: '150px', borderRadius: '50%'}}>
                     <div>
                       <h3>{v.title}</h3>
                     </div>
